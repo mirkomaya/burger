6c6f2ddc45c9993c33c89692ca05cd6dca8ce6a2
Marty McFly <mirko.maya@live.com>
2019-10-26 01:31:24 -0400

    Working on orm.js.

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name)
+VALUES ("Luger Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Le Pigeon Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Double Animal Style Burger");
